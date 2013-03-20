f1908e52f93a19a36ac137df61d929fb5e13211c
geniusfox <geniusfox@gmail.com>
2013-03-20 10:54:37 +0800

    20130320

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-  attr_accessible :name,:telphone, :tutor_id,:leader_id,:is_leader
+  attr_accessible :name,:telphone, :tutor_id,:leader_id,:is_leader,:email
   belongs_to :tutor
   belongs_to :leader, :class_name=>"Member",:foreign_key=>'leader_id'
 
